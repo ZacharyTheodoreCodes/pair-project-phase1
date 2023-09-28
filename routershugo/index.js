b3dfe6816037e51f708cf0f21da0ed4af10f2b8b
AsmodayCelestia <herusyeme@gmail.com>
2023-09-28 12:54:40 +0700

    hugoejs

--- routershugo/index.js
+++ routershugo/index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 // middleware that is specific to this router
-// router.use((req, res, next) => {
-//   console.log('Time: ', Date.now())
-//   next()
-// })
+router.use((req, res, next) => {
+  console.log('Time: ', Date.now())
+  next()
+})
 // define the home page route
-router.get('/login', (req, res) => {
-  res.render('add', {'title': "loginPage"})
+router.get('/', (req, res) => {
+  res.send('Birds home page')
 })
 // define the about route
 router.get('/about', (req, res) => {
