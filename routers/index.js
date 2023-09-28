49d29322f3e6504ea08e151d55cdf68ef509a2c6
AsmodayCelestia <herusyeme@gmail.com>
2023-09-28 12:42:08 +0700

    ejs

--- routers/index.js
+++ routers/index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 // middleware that is specific to this router
-router.use((req, res, next) => {
-  console.log('Time: ', Date.now())
-  next()
-})
+// router.use((req, res, next) => {
+//   console.log('Time: ', Date.now())
+//   next()
+// })
 // define the home page route
-router.get('/', (req, res) => {
-  res.send('Birds home page')
+router.get('/login', (req, res) => {
+  res.render('add', {'title': "loginPage"})
 })
 // define the about route
 router.get('/about', (req, res) => {
